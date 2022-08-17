511e13864ec4645993cd2c89759b519eb983a2ef
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-08-16 20:50:59 -0500

    todo list

---
+++ todo.js
@@ -0,0 +1,43 @@
+// Polish:
+// color
+// tweening (position, scale, rotation, speed)
+// squash + stretch
+// hitflash
+// freezeframe
+// slowmo
+// particles
+// shake
+// personality
+
+// ============
+// POLISH TODOS
+// ============
+// SHOOT
+// TODO: add recoil
+// TODO: spawn particles
+
+// JUMP
+// TODO: implement jump arc
+// TODO: particle dust on land
+
+// MOVEMENT
+// TODO: implement movement tweening (player, shots and enemy)
+
+// SPAWNS
+// TODO: avoid spawning near player
+
+// COLLISION:
+// TODO: favor player on hitboxes
+// TODO: spawn particles on destruction/hit
+
+// CAMERA
+// TODO: shake on shots, destruction, and hits
+// TODO: add mild tweening for shakes
+
+// ============
+// DESIGN TODOS
+// ============
+// TODO: player can easily avoid all comets on certain map areas (fix)
+// TODO: player needs more reason to move around the map
+// TODO: need more confirmation of when player takes a hit
+// TODO: visually indicate the lifetime of a collectible
