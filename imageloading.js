f91308c01c10e901c76473c081e2842167a93b5c
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-09-06 15:57:22 -0500

    hp recovery sprite

--- imageloading.js
+++ imageloading.js
@@ -19,6 +19,7 @@ const image_list = [
   { file: "glow.png", name: "glow" },
   { file: "player-spritesheet.png", name: "player_sheet" },
   { file: "collectible.png", name: "collectible" },
+  { file: "hp-up.png", name: "hp_up" },
   // <-- Add your sprite in here
 ];
 
