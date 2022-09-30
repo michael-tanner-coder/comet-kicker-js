c8f68682f9b3a57794a5c78ae4e560ee14139563
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-09-30 16:20:59 -0500

    Separate file for player jump logic

---
+++ js/game/playerjump.js
@@ -0,0 +1,17 @@
+function gatherInput() {}
+
+function runCollisionChecks() {}
+
+function calculateRayRanged() {}
+
+function evaluateRayPositions(range) {}
+
+function calculateWalk() {}
+
+function calculateGravity() {}
+
+function calculateJumpApex() {}
+
+function calculateJump() {}
+
+function moveCharacter() {}
