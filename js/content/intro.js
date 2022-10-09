2b375dc9599cebe24908995d643060115aba42c2
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-10-09 17:00:51 -0500

    Script draft for intro sequence

---
+++ js/content/intro.js
@@ -0,0 +1,31 @@
+/*
+Long ago, deep in the far reaches of space, 
+
+there lived a young boy known only as “Kicker”. 
+
+In his youth, Kicker discovered he had immense, near-infinite power 
+
+- enough power to destroy the world around him 
+
+- and even worlds beyond his own. 
+
+To keep his loved ones safe, Kicker isolated himself to train every day, hoping he could control his powers before they harmed anyone else.
+
+But in his absence, a dark force crept over their galaxy. 
+
+The Agents of the Cosmos - followers of the Big Star - were determined to take control of every planet across the universe.
+
+Now Kicker must emerge from his training to defend his home
+
+Was his training enough? 
+
+Can he control his powers and save the galaxy? 
+
+Or will he only destroy himself and everyone around him?
+
+Is he powerful enough to become … 
+
+[cut to title screen]
+
+COMET KICKER
+*/
