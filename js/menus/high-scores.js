7561116f0c6dd1ef836f38372edea09342782fb5
Mike DiGiovanni <vinblau@gmail.com>
2022-10-10 10:12:57 -0400

    High Score menu will no longer freeze game for a new player

--- js/menus/high-scores.js
+++ js/menus/high-scores.js
@@ -1,12 +1,14 @@
 const scores = JSON.parse(localStorage.getItem("high_scores"));
 const score_list = [];
-scores.forEach((score, i) => {
-  let score_text = i + 1 + ". " + Math.floor(score);
-  let new_text = new Text();
-  new_text.update = () => {};
-  new_text.text = score_text;
-  score_list.push(new_text);
-});
+if (scores) {
+  scores.forEach((score, i) => {
+    let score_text = i + 1 + ". " + Math.floor(score);
+    let new_text = new Text();
+    new_text.update = () => {};
+    new_text.text = score_text;
+    score_list.push(new_text);
+  });
+}
 createMenu({
   id: "highScoreMenu",
   key: "high_scores",
