24546d2b838ca7116b3f77c43600809bd7f55819
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-10-17 21:17:41 -0500

    Updating todo list

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
-// Polish:
-// color
+// Polish Effects:
 // tweening (position, scale, rotation, speed)
 // squash + stretch
 // hitflash
@@ -10,23 +9,33 @@
 // personality
 
 // ============
-// POLISH TODOS
+// FINAL TODOS
 // ============
-// MOVEMENT
-// TODO: implement movement tweening (player, shots and enemy)
-
-// SPAWNS
 // TODO: avoid spawning near player
-
-// COLLISION:
-// TODO: favor player on hitboxes
-// TODO: spawn particles on destruction/hit
+// TODO: favor player on hitboxes (larger shot hitboxes, smaller player hitbox)
 // TODO: stop player from tunneling into walls
-
-// ============
-// DESIGN TODOS
-// ============
 // TODO: player can easily avoid all comets on certain map areas (fix)
 // TODO: player needs more reason to move around the map
 // TODO: need more confirmation of when player takes a hit
 // TODO: visually indicate the lifetime of a collectible
+// TODO: slightly slow down game when enemies are near player to give them a chance to move
+// TODO: balance enemy movement speed
+// TODO: assign points to powerups for unlocking them (connect to save data)
+// TODO: edit the menu theme
+// TODO: replace health bars with heart icons
+// TODO: set max health?
+// TODO: limit health via difficulty mode?
+// TODO: set a point limit to unlock the ending sequence
+// TODO: write ending sequence + add images
+// TODO: track unlockables via menu
+// TODO: track unlockables via menu
+// TODO: remove whitebox from hitflash, brighten sprite instead
+// TODO: slow down game on hit
+// TODO: adjust spawn rate over time (tension + release)
+// TODO: assign different point values to enemies (more points for tougher enemies)
+// TODO: hitstop on gameover, switch to game over sprite
+// TODO: prevent player from sliding when under a platform
+// TODO: implement smooth cornering for player and enemies
+// TODO: screenwrap shots
+// TODO: fix pause menu
+// TODO: update text on game over menu
