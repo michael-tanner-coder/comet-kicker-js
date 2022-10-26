7a2cfc92a27559030d29afcf3673fe3b0bb633d1
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-10-26 00:35:19 -0500

    Object structure for unlockables

---
+++ js/content/unlockables.js
@@ -0,0 +1,17 @@
+const UNLOCK_EFFECTS = {
+  HP: "hp",
+  POWERUP: "powerup",
+  POWERUP_TIME: "powerup_time",
+  MULTIPLIER: "multiplier",
+  POINT_BOXES: "",
+};
+
+const UNLOCKABLE = {
+  points: 0,
+  name: "",
+  is_unlocked: false,
+  powerup: null,
+  effect: "",
+};
+
+const UNLOCKED = [];
