ee0f42a1fc6a6e42c6734fa7251d7b37158eb95f
Gamkedo <chris@gamkedo.com>
2022-10-28 20:56:51 -0400

    Credits integrated as of today

--- js/menus/menu.js
+++ js/menus/menu.js
@@ -1,4 +1,4 @@
-this.creditsShowHack = false;
+var globalCreditsShowHack = false;
 class Menu {
   constructor(props) {
     this.id = props?.id || "";
@@ -101,10 +101,75 @@ class Menu {
       drawCenteredText(this.no_options_text, GAME_H / 2);
     }
 
-    // Footer
-    if (this.id !== "mainMenu") {
+    if(globalCreditsShowHack) {
+      context.fillStyle = WHITE;
+      var wasFont = context.font;
+      context.font = "5px Arial"
+      for(var i=0;i<creditsList.length;i++) {
+        context.fillText(creditsList[i], 13, 7+i*8);
+      }
+      context.font = wasFont;
+    } else if (this.id !== "mainMenu") { // Footer
       context.fillStyle = WHITE;
       context.fillText("Space = go back", 16, 16);
     }
   }
 }
+
+var creditsList = [
+"Michael Monty: Project lead, core gameplay, player control, main sprites and related animations (platform, player, enemy, shots), title and intro music, Spanish and French translations, powerups, invulnerability frames, score tracking, parallax backgrounds, UI, intro sequence including images, audio and animation code, sounds (explosion, shot), menu system, tuning, color trails, logo, control remapping",
+"Patrick Moffett: New jump physics, wall sticking, drop through platform (firing up), save/restore of input and sound settings, assorted fixes (sound menu, high refresh rate input, audio during pause or game over, missiles bug), game over to reset, low pass filter, util functions",
+"mike dg: Color selection, various fixes (high score freeze, device detection, high refresh rate physics, spawn timers, collectible duration, move consistency, spawn frequency)",
+"Rodrigo Bonzerr Lopez: Future music, missile art, collection sound, lose hp sound, rotating shield sprites",
+"Christer \"McFunkypants\" Kaitila: Gamepad, screenshake, particles (dust, smoke, fire, sparkle, glow, explosion), title screen decoration, score initialization bug correction, collision fix, blurry font fix",
+"Jared Rigby: Chinese translation, shield hit noise, pause system",
+"Playtesters: Klaim (A. Joël Lamotte), Cassidy Noble, Patrick J Thompson, mike dg, Patrick McKeown, Rodrigo Bonzerr Lopez, Rohit Narwal Kumar, Tor Andreas Johnsen",
+"=== PRESS SPACEBAR to GO BACK ==="
+];
+
+function lineWrapCredits() { // note: gets calling immediately after definition!
+  const newCut = [];
+  var maxLineChar = 57;
+  var findEnd;
+
+  for(let i = 0; i < creditsList.length; i++) {
+    const currentLine = creditsList[i];
+    for(let j = 0; j < currentLine.length; j++) {
+      /*const aChar = currentLine[j];
+      if(aChar === ":") {
+        if(i !== 0) {
+          newCut.push("\n");
+        }
+        newCut.push(currentLine.substring(0, j + 1));
+        newCut.push(currentLine.substring(j + 2, currentLine.length));
+        break;
+      } else*/ if(j === currentLine.length - 1) {
+        if((i === 0) || (i >= creditsList.length - 2)) {
+          newCut.push(currentLine);
+        } else {
+          newCut.push(currentLine.substring(0, currentLine.length));
+        }
+      }
+    }
+  }
+
+  const newerCut = [];
+  for(var i=0;i<newCut.length;i++) {
+    while(newCut[i].length > 0) {
+      findEnd = maxLineChar;
+      if(newCut[i].length > maxLineChar) {
+        for(var ii=findEnd;ii>0;ii--) {
+          if(newCut[i].charAt(ii) == " ") {
+            findEnd=ii;
+            break;
+          }
+        }
+      }
+      newerCut.push(newCut[i].substring(0, findEnd));
+      newCut[i] = newCut[i].substring(findEnd, newCut[i].length);
+    }
+  }
+
+  creditsList = newerCut;
+}
+lineWrapCredits(); // note: calling immediately as part of init, outside the function
