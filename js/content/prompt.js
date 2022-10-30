62f80f161451aab9a1e0319b127ad1450e5b744d
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2022-10-29 23:12:47 -0500

    Created a list of tutorial prompts

--- js/content/prompt.js
+++ js/content/prompt.js
@@ -5,13 +5,50 @@ const PROMPT = {
   h: 32,
   color: WHITE,
   text: "SHOOT: ",
-  control: "shoot",
+  controls: ["shoot", "moveUp", "moveDown"],
+};
+
+// CONTROL PROMPTS
+const SHOOT_PROMPT = {
+  ...PROMPT,
+  text: "SHOOT: ",
+  controls: ["shoot"],
+};
+const SHORT_JUMP_PROMPT = {
+  ...PROMPT,
+  text: "SHORT JUMP (tap): ",
+  controls: ["jump"],
+};
+const LONG_JUMP_PROMPT = {
+  ...PROMPT,
+  text: "LONG JUMP (hold): ",
+  controls: ["jump"],
+};
+const MOVE_LEFT_PROMPT = {
+  ...PROMPT,
+  text: "MOVE LEFT: ",
+  controls: ["moveLeft"],
+};
+const MOVE_RIGHT_PROMPT = {
+  ...PROMPT,
+  text: "MOVE RIGHT: ",
+  controls: ["moveRight"],
+};
+const SHOOT_UP_PROMPT = {
+  ...PROMPT,
+  text: "SHOOT UP: ",
+  controls: ["shoot", "moveUp"],
+};
+const SHOOT_DOWN_PROMPT = {
+  ...PROMPT,
+  text: "SHOOT UP: ",
+  controls: ["shoot", "moveDown"],
 };
 
 const drawPrompt = (prompt) => {
   prompt.w = context.measureText(prompt.text).width;
   prompt.x = GAME_W / 2 - prompt.w / 2;
-  prompt.y = GAME_H - prompt.h + 16;
+  prompt.y = GAME_H - prompt.h + 22;
 
   prompt.x = Math.floor(prompt.x);
   prompt.y = Math.floor(prompt.y);
@@ -22,10 +59,15 @@ const drawPrompt = (prompt) => {
   context.fillStyle = prompt.color;
   context.fillText(prompt.text, prompt.x, prompt.y);
 
-  playAnimation(
-    getInputAnimation(CONTROLS[prompt.control]),
-    1,
-    prompt.x + prompt.w,
-    prompt.y - 12
-  );
+  prompt.controls.forEach((control, i) => {
+    playAnimation(
+      getInputAnimation(CONTROLS[control]),
+      1,
+      prompt.x + prompt.w + 32 * i,
+      prompt.y - 12
+    );
+    if (i !== prompt.controls.length - 1) {
+      context.fillText("+", prompt.x + prompt.w + 32 * i + 20, prompt.y);
+    }
+  });
 };
