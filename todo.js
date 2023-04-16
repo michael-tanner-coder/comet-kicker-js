f76c6c6d2087b641bc2d3d76b705bb188899693f
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-04-16 17:41:31 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -16,4 +16,6 @@
 // TODO: add tutorial prompt for score falling + screenwrapping
 // TODO: fix when inputs are not detected continuously from gamepad
 // TODO: fix color change option
+// TODO: fix bug where two powerups can be gained at once
+// TODO: fix bug where powerups don't spawn again if there is only one unlocked 
 
