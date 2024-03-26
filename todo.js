dabe3fdbaaa250eb3e2476372f2c267b4c8e03bb
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-03-25 22:24:45 -0700

    categorizing todos

--- todo.js
+++ todo.js
@@ -11,8 +11,33 @@
 // ===========
 // FINAL TODOS
 // ===========
-// TODO: track unlockables via menu and gameoverui
-// TODO: fix bug where two powerups can be gained at once
+
+// TEST IF THESE ARE STILL AN ISSUE
 // TODO: fix color change option
 // TODO: fix when inputs are not detected continuously from gamepad
+
+// SCORING:
+// TODO: stop the score drop when the player gets a kill
+// TODO: show the points added to the main score
+// TODO: shake the main score text for impact
+
+// WEAPONS:
+// TODO: experiment with rapid fire spread
+// TODO: faster rapid fire with diminishing recoil over time
+// TODO: fix bug where two powerups can be gained at once
+
+// FEEDBACK:
+// TODO: emit flash radial animation when hit
+// TODO: stop horizontal animation when hit
+// TODO: flash player sprite during iframes
+// TODO: give player and enemies a white silhouette sprite when screenwrapping
+
+// TUTORIALIZATION:
+// TODO: create a new tutorial level that can be played via menu or after the opening cutscene
+// TODO: put prompts in the center of the screen
+// TODO: in tutorial, require three successful attempts of any technique before progressing
+
+// UI:
+// TODO: track unlockables via menu and gameoverui
 // TODO: refresh UI when data is deleted
+// TODO: use Hold to Confirm when deleting data or moving through unlock menu
